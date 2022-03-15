0f4453d4b3baeb2234d61944c9e0f15d37a3fa7e
Rebecca Snell <snell@ohio.edu>
2022-03-15 17:01:31 -0400

    Added seedWeight data and graphed it

--- simpleRcode.R
+++ simpleRcode.R
@@ -6,3 +6,11 @@ y <- runif(20, min = 1, max = 2)
 testDat <- data.frame(x,y)
 
 plot(testDat)
+
+seedData <- read.csv("~/Teaching/PBIO_3150/Labs/seedWeight.csv")
+
+library(ggplot2)
+ggplot(seedData, aes(x = treatment, y = seedWeight))+geom_boxplot()+
+  theme_bw() +
+  xlab("Treatment")+
+  ylab("Weight of seeds")
